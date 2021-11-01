c6b23761d3ab6315f23ae328196a5e9463d49927
Corbett <corbett.croslin@gmail.com>
2021-10-31 20:40:40 -0400

    new

---
+++ app.js
@@ -0,0 +1,127 @@
+// 1
+
+let yourArray = [1, "dog", true, 21, "cat"]
+
+// 2
+
+let myArray = ["a", "x", "c", "d"];
+// Only change code below this line
+let ourVarible = myArray[3];
+myArray[3] = "d";
+// Only change code above this line
+console.log(myArray);
+
+// 3
+
+arr.push(7, 'VIII', 9);
+arr.unshift('I', 2, 'three');
+
+// 4
+
+function popShift(arr) {
+    let popped = arr.pop();
+    let shifted = arr.shift();
+    return [shifted, popped];
+  }
+  
+  console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+//5
+
+arr.splice(1, 4)
+
+// 6
+
+function htmlColorNames(arr) {
+    // Only change code below this line
+  arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond')
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+//  7
+
+return arr.slice(2,4);
+
+
+
+
+// // 1
+
+// //This is an in-line comment
+// /* this is a 
+// multi-line comment */
+
+// // 2
+
+// var myName;
+
+// // 3
+
+// var a;
+// a = 7;
+
+// // 4
+
+// b = a
+
+// // 5
+
+// var myVar = 0;
+// var a = 9;
+
+// // 6
+
+// var myFirstName = 'Corbett';
+// var myLastName = 'Croslin';
+
+// // 7
+
+// var a = 5;
+// var b = 10;
+// var c ="I am a";
+
+// //8
+
+// // Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+// // 9
+
+// let catName = "Oliver";
+// let catSound = "Meow!";
+
+// // 10
+
+// const FCC = "freeCodeCamp";
+// let fact = "is cool!";
+// // Only change code above this line
+
+// fact = "is awesome!";
+// console.log(FCC, fact);
+
+// // 11
+
+// const sum = 15 + 5;
+
+// // 12
+
+// const difference = 45 - 33;
+
+// // 13
+
+// const product = 8 * 10;
+
+// // 14
+
+
+
